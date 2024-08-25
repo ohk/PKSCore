e86c6dcdb9aa3739096fa7d3c373872da6d62eac
Omer Hamid Kamisli <ohkamisli@poikus.com>
2024-08-25 22:06:13 +0300

    feat: Add convenience properties for next day and previous day in Date extension

--- PKSFoundation/Sources/Extensions/Date+Ext.swift
+++ PKSFoundation/Sources/Extensions/Date+Ext.swift
@@ -7,22 +7,72 @@
 
 import Foundation
 
+@available(iOS 12.0, macOS 10.12, tvOS 12.0, watchOS 4.0, visionOS 1.0, *)
 public extension Date {
-    /**
-     Returns the date representing tomorrow.
-     
-     - Returns: The date representing tomorrow.
-     */
-    var tomorrow: Date {
+    
+    /// The date representing the next day.
+    ///
+    /// This property calculates the date that is exactly one day after the current date.
+    /// If the calculation fails, it returns the current date.
+    ///
+    /// Example usage:
+    ///
+    /// ```swift
+    /// let today = Date()
+    /// let nextDay = today.nextDay
+    /// ```
+    ///
+    /// - Returns: A `Date` instance representing the next day.
+    var nextDay: Date {
         Calendar.current.date(byAdding: .day, value: 1, to: self) ?? self
     }
 
-    /**
-     Returns the date representing yesterday.
-     
-     - Returns: The date representing yesterday.
-     */
-    var yesterday: Date {
+    /// The date representing the previous day.
+    ///
+    /// This property calculates the date that is exactly one day before the current date.
+    /// If the calculation fails, it returns the current date.
+    ///
+    /// Example usage:
+    ///
+    /// ```swift
+    /// let today = Date()
+    /// let previousDay = today.previousDay
+    /// ```
+    ///
+    /// - Returns: A `Date` instance representing the previous day.
+    var previousDay: Date {
         Calendar.current.date(byAdding: .day, value: -1, to: self) ?? self
+    }
+
+    /// The date representing tomorrow.
+    ///
+    /// This property calculates the date that is exactly one day after the current date.
+    /// It is a convenience property that uses the `nextDay` property.
+    ///
+    /// Example usage:
+    ///
+    /// ```swift
+    /// let tomorrow = Date().tomorrow
+    /// ```
+    ///
+    /// - Returns: A `Date` instance representing tomorrow.
+    var tomorrow: Date {
+        Date().nextDay
+    }
+
+    /// The date representing yesterday.
+    ///
+    /// This property calculates the date that is exactly one day before the current date.
+    /// It is a convenience property that uses the `previousDay` property.
+    ///
+    /// Example usage:
+    ///
+    /// ```swift
+    /// let yesterday = Date().yesterday
+    /// ```
+    ///
+    /// - Returns: A `Date` instance representing yesterday.
+    var yesterday: Date {
+        Date().previousDay
     }
 }
