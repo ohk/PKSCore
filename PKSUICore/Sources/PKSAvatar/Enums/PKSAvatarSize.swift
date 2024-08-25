aa26dbacd6fa381e4a33713294f941e1347ff66d
Omer Hamid Kamisli <ohkamisli@poikus.com>
2024-08-25 22:26:24 +0300

    refactor: Update PKSAvatarSize enum to conform to Identifiable and Equatable protocols

--- PKSUICore/Sources/PKSAvatar/Enums/PKSAvatarSize.swift
+++ PKSUICore/Sources/PKSAvatar/Enums/PKSAvatarSize.swift
@@ -7,7 +7,31 @@
 
 import Foundation
 
-public enum PKSAvatarSize {
+public enum PKSAvatarSize: CaseIterable, Identifiable, Equatable {
+    public static var allCases: [PKSAvatarSize] = [.small, .medium, .large, .xLarge, .xxLarge, .fill]
+    
+    public var id: String {
+        var baseID: String = "PKSAvatarSize"
+        switch self {
+        case .small:
+            baseID += "SM"
+        case .medium:
+            baseID += "MD"
+        case .large:
+            baseID += "LG"
+        case .xLarge:
+            baseID += "XL"
+        case .xxLarge:
+            baseID += "XXL"
+        case .fill:
+            baseID += "FILL"
+        case .custom(let width, let height):
+            baseID += "CUSTOM\(width)x\(height)"
+        }
+        
+        return baseID
+    }
+    
     case small
     case medium
     case large
