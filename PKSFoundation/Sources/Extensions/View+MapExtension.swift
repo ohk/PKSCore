31f597e14ccc0d9b8135aa989d0e6e77ed2367a7
Omer Hamid Kamisli <ohkamisli@poikus.com>
2024-08-25 21:41:48 +0300

    feat: Add View+MapExtension.swift for transforming views

---
+++ PKSFoundation/Sources/Extensions/View+MapExtension.swift
@@ -0,0 +1,28 @@
+//
+//  View+MapExtension.swift
+//
+//
+//  Created by Ömer Hamid Kamışlı on 8/25/24.
+//
+
+import SwiftUI
+
+extension View {
+    /// Transforms the view into another view using a closure.
+    ///
+    /// Use the `map(_:)` function to apply a transformation to a view,
+    /// returning a new view type based on the original view.
+    ///
+    /// Example usage:
+    ///
+    /// ```swift
+    /// let transformedView = Text("Hello, World!").map { $0.foregroundColor(.red) }
+    /// ```
+    ///
+    /// - Parameter transform: A closure that takes the original view as input
+    ///   and returns a new view.
+    /// - Returns: A new view type resulting from the transformation.
+    func map<T: View>(_ transform: @escaping (Self) -> T) -> T {
+        return transform(self)
+    }
+}
