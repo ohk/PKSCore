28a45986d044494350c319a7caee1b82e5e16303
Omer Hamid Kamisli <ohkamisli@poikus.com>
2024-08-25 22:07:54 +0300

    feat: Add PKSRadius enum for defining radius sizes

---
+++ PKSUICore/Sources/Shared/Radius/PKSRadius.swift
@@ -0,0 +1,17 @@
+//
+//  PKSRadius.swift
+//
+//
+//  Created by Ömer Hamid Kamışlı on 8/24/24.
+//
+
+import Foundation
+
+public enum PKSRadius {
+    case sm
+    case md
+    case lg
+    case xl
+    case xxl
+    case full
+}
