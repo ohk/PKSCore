4cfe4f6946d7a62d9d78957da5794e8b74136e08
Omer Hamid Kamisli <ohkamisli@poikus.com>
2024-08-26 17:10:07 +0300

    refactor: Update AvatarColorMode enum to include new color modes

--- PKSUICore/Sources/PKSAvatar/Enums/AvatarColorMode.swift
+++ PKSUICore/Sources/PKSAvatar/Enums/AvatarColorMode.swift
@@ -7,7 +7,24 @@
 
 import SwiftUI
 
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public enum AvatarColorMode: Equatable {
+    
+    /// A mode that selects a random color.
+    ///
+    /// Use this mode when you want the color to be chosen randomly.
     case random
+    
+    /// A mode that uses a custom color.
+    ///
+    /// Use this mode to specify a custom color that should be used.
+    ///
+    /// - Parameter color: The custom `Color` to be used.
     case custom(color: Color)
+    
+    /// A mode that selects a color that provides the best visibility.
+    ///
+    /// Use this mode when you want the color to be automatically chosen to ensure the best contrast with the background.
+    case visible
 }
+
